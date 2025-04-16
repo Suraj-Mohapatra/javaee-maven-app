de7bd3a227ea8b23192f117786a41b91e6d8c707
Suraj-Mohapatra <suraj2895@gmail.com>
2025-04-17 02:54:22 +0530

    adds a servlet

---
+++ src/main/java/com/ugleagle/controller/LoginServlet.java
@@ -0,0 +1,56 @@
+package com.ugleagle.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Suraj Mohapatra
+ */
+
+// for class LoginServlet, see the servlet configuration in deployment descriptor i.e. web.xml
+public class LoginServlet extends HttpServlet {
+    
+    public LoginServlet(){
+        // explicit declaration & definition of no args constructor is not necessary
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
